16021a3f4b26f33522e3802980b33cb33f464550
duocld <duyduoc03bg@gmail.com>
2024-12-05 20:27:54 +0700

    buổi 3

---
+++ Bai3_Assignment/src/Ex1/Student.java
@@ -0,0 +1,36 @@
+package Ex1;
+
+public class Student {
+    private static int idCounter = 0;
+    private int id;
+    private String name;
+
+    public Student(String name) {
+        this.id = ++idCounter;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
